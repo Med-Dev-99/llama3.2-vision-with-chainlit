8945f709839145c11411576eee02c237a39db06c
Med-Dev-99 <elkorchimohameddev@gmail.com>
2024-11-11 12:25:07 +0100

    ADD test.py for testing

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import ollama
 
-with open('/home/meddev/Pictures/perso_data/WhatsApp Image 2024-10-21 at 10.00.47.jpeg', 'rb') as file:
+with open('image.jpg', 'rb') as file:
   response = ollama.chat(
     model='llama3.2-vision',
     messages=[
